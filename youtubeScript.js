9704a75df4e5d9c28f877ad499335ac5917d245e
Darye <darioperez047@gmail.com>
2023-03-06 19:31:06 +0100

    ytPremiumAdDeleted log deleted, fix event.path, fix popupScript

--- youtubeScript.js
+++ youtubeScript.js
@@ -1,26 +1,29 @@
 ytPremiumPopupDeleted = false;
 document.addEventListener("click", function(event) {
-    event.path.every(v => {
+    element = event.target
+    while (true) {
         try {
-            if (v.nodeName.toLowerCase().includes("download")) {
+            if (element.nodeName.toLowerCase().includes("download")) {
                 if (!ytPremiumPopupDeleted) {
                     myTimeout = setInterval(function (params) {
                         try {
                             document.querySelector("tp-yt-paper-dialog").remove();
                             clearInterval(myTimeout);
                             ytPremiumPopupDeleted = true;
-                            console.log("deleted")
                         } catch (error) {}
                     }, 500);
                 } else {
-                    console.log("alreadyDeleted")
                 }
                 chrome.runtime.sendMessage({url: location.href});
-                return false;
+                break;
+            } else {
+                element = element.parentNode;
             }
-        } catch (error) {}
-        return true;
-    });
+        } catch (error) {
+            console.log(error);
+            break;
+        }
+    }
 });
 
 console.log('[Cobalto] Youtube Script injected! Press the "Download" button on a video to open Cobalto\'s prompt.')
